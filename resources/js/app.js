a7fb7654fbf872fcccc1f6b1f51100fce09a7310
lucans <luuucans@gmail.com>
2016-07-26 01:18:25 -0300

    Medíocre

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,19 +15,45 @@ var app = angular.module('cock', ['ui.router'])
         })
 }) 
 
-app.controller("homeCtrl", ['$scope', '$http', '$rootScope', function ($s, $http, $rs) {                  
-   $s.teste = function(){
-    console.log("testando 2");
-  }
-  $s.teste();
+.service('DateProvider', function () {
+
+    this.date = new Date();
+    this.payment = new Date();
+    
+    this.today = function () {
+        return this.date.getDate();
+    }
+
+    this.dayOfWeek = function () {
+        return this.date.getDay();
+    }
+
+    this.getHour = function (){
+        return this.date.getHours() + ':' + this.date.getMinutes();
+    } 
+
+    // this.getHourMinute = function (){
+    //     return this.date.getHours() + ':' + this.date.getMinutes();
+    // }    
+
+    // this.getHourMinuteSecond = function (){
+    //     return this.date.getHours() + ':' + this.date.getMinutes();
+    // }
+
+    this.toPayDay = function (){
+        return this.payment +  this.date;
+    };
+
+});
+
+app.controller("homeCtrl", ['$scope', '$http', '$rootScope','DateProvider', function ($s, $http, $rs, Date) {  
+  
+    $s.today = Date.toPayDay();
+    console.log($s.today);
+
 }]);
 
 app.controller("menuCtrl", ['$scope', '$http', '$rootScope', function ($s, $http, $rs) {                  
-  
-  $s.teste = function(){
-    console.log("testando");
-  }
-  $s.teste();
 
 }]);
 
